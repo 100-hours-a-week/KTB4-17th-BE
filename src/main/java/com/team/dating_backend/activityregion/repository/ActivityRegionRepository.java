bf93f8abcfdcbe7372f2b231d635e2a2dd35726d
gpbz56 <d45h1gk@kangnam.ac.kr>
2026-09-20 18:18:24 +0900

    feat: 활동 지역 기준 데이터 초기 적재 추가

---
+++ src/main/java/com/team/dating_backend/activityregion/repository/ActivityRegionRepository.java
@@ -0,0 +1,6 @@
+package com.team.dating_backend.activityregion.repository;
+
+import com.team.dating_backend.activityregion.entity.ActivityRegion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRegionRepository extends JpaRepository<ActivityRegion, Long> {}
